e39c2310d7911135c0934da07e80775b10053e57
eamonn-zh <eamonn0223@gmail.com>
2023-01-06 22:50:30 -0800

    fix indexing bug

--- lib/joint/solver_3djcg.py
+++ lib/joint/solver_3djcg.py
@@ -90,6 +90,7 @@ EPOCH_REPORT_TEMPLATE = """
 [val]   val_pos_ratio: {val_pos_ratio}, val_neg_ratio: {val_neg_ratio}
 [val]   val_iou_rate_0.25: {val_iou_rate_25}, val_iou_rate_0.5: {val_iou_rate_5}
 [val]   val_max_iou_rate_0.25: {val_max_iou_rate_25}, val_max_iou_rate_0.5: {val_max_iou_rate_5}
+[val]   scanrefer_plus_overall_25: {scanrefer++_overall_25}, scanrefer_plus_overall_50: {scanrefer++_overall_50},
 """
 
 BEST_REPORT_TEMPLATE = """
@@ -804,6 +805,7 @@ class Solver():
                             json.dump(value, f)
 
                     all_preds, all_gts = load_gt_and_pred_jsons_from_disk("scanrefer++_test", "3dvg_gt")
+                    print("?????????????????????????????????????????????????????????????????")
                     iou_25_results, iou_50_results = evaluate_all_scenes(all_preds, all_gts)
                     self.log[phase]["scanrefer++_overall_25"] = iou_25_results["overall"]
                     self.log[phase]["scanrefer++_overall_50"] = iou_50_results["overall"]
@@ -1016,6 +1018,8 @@ class Solver():
             val_iou_rate_5=round(np.mean([v for v in self.log["val"]["iou_rate_0.5"]]), 5),
             val_max_iou_rate_25=round(np.mean([v for v in self.log["val"]["max_iou_rate_0.25"]]), 5),
             val_max_iou_rate_5=round(np.mean([v for v in self.log["val"]["max_iou_rate_0.5"]]), 5),
+            scanrefer_plus_overall_25=self.log["val"]["scanrefer++_overall_25"],
+            scanrefer_plus_overall_50=self.log["val"]["scanrefer++_overall_50"],
         )
         self._log(epoch_report)
         self._log_eval(epoch_report)
